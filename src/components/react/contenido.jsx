834ee5955f33cfaf0e6925c29d5b24c50497aa27
25JT <jhonierlucumimeza@gmail.com>
2025-10-02 09:58:23 -0500

    Update contenido.jsx

---
+++ src/components/react/contenido.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Contenido = () => {
+    return (
+        <div>
+            <h1>Contenido</h1>
+        </div>
+    );
+};
+
+export default Contenido;
